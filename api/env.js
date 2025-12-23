aea1326953094ddcaffb59d530a0fc6bc52d45de
tvclena <tvclena@gmail.com>
2025-12-23 03:22:42 -0300

    Delete api/env.js

--- api/env.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  });
-}
