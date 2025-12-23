920f71c76d5db6510e584fe143819357e56ceb57
tvclena <tvclena@gmail.com>
2025-12-23 03:04:42 -0300

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,4 @@
 window.ENV = {
   SUPABASE_URL: "https://dxkszikemntfusfyrzos.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_xxxxxxxxx",
-  BUNNY_PULL_ZONE: "https://sessao99.b-cdn.net"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
 };
