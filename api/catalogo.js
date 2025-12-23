254d2cd861cb324be66312b33e8622ed4c45bec1
tvclena <tvclena@gmail.com>
2025-12-23 19:21:44 -0300

    Create catalogo.js

---
+++ api/catalogo.js
@@ -0,0 +1,20 @@
+// api/catalogo.js
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
+export default async function handler(req, res) {
+  const { data, error } = await supabase
+    .from("filmes")
+    .select("*")
+    .eq("ativo", true);
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.json(data);
+}
