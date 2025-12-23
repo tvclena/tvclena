d952b81aacc469192cf1d697bda0d05bbcaffc1c
tvclena <tvclena@gmail.com>
2025-12-23 19:59:45 -0300

    Update validar-username.js

--- api/validar-username.js
+++ api/validar-username.js
@@ -6,29 +6,14 @@ const supabase = createClient(
 );
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Método inválido" });
-  }
-
   const { email, username } = req.body;
-
-  if (!username) {
-    return res.status(400).json({ disponivel: false });
-  }
 
   const { data } = await supabase
     .from("usuarios")
-    .select("id,email")
+    .select("id")
     .eq("username", username)
+    .neq("email", email)
     .maybeSingle();
 
-  if (!data) {
-    return res.json({ disponivel: true });
-  }
-
-  if (data.email === email) {
-    return res.json({ disponivel: true });
-  }
-
-  return res.json({ disponivel: false });
+  res.json({ disponivel: !data });
 }
