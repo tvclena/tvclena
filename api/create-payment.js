bc0e0cdb2968e18b92516e594d2250761bddde01
tvclena <tvclena@gmail.com>
2025-12-25 12:32:56 -0300

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -2,8 +2,6 @@ export const config = {
   runtime: "nodejs"
 };
 
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).end();
