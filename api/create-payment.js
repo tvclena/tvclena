91418895fd8eb3cbb3f146bc506dab09efc13dc2
tvclena <tvclena@gmail.com>
2025-12-28 19:56:25 -0300

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -12,7 +12,7 @@ const sb = createClient(
 export default async function handler(req, res) {
   try {
 
-    //  LISTAR APEX
+    // LISTAR APEX
     if (req.method === "POST" && req.body?.action === "list_apex") {
       const { data, error } = await sb
         .from("planos")
