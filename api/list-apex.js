9064d0939395061548a572a0a5307c56539422eb
tvclena <tvclena@gmail.com>
2025-12-29 14:15:07 -0300

    Update list-apex.js

--- api/list-apex.js
+++ api/list-apex.js
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
-
   const { data, error } = await sb
     .from("planos")
     .select("id, valor")
